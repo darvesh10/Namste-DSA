62739f3c8a76cb492a8f8c11f11a15994e38d300
Darvesh Soni <darveshsoni1@gmail.com>
2025-07-07 17:26:57 +0530

    Add files via upload

---
+++ Reverse Linked List/index.js
@@ -0,0 +1,12 @@
+function reverse(head){
+    let prev = null;
+    let curr = head;
+    while(curr!==null){
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    head=prev;
+    return prev;
+}
