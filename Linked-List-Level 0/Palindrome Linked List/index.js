ea2ab2fd1ce443a64f6ee3cd7a75f0845065cb87
Darvesh Soni <darveshsoni1@gmail.com>
2025-07-20 16:36:55 +0530

    Add files via upload

---
+++ Linked-List-Level 0/Palindrome Linked List/index.js
@@ -0,0 +1,14 @@
+function plaindrome(head) {
+    let arr = [];
+    let curr = head;
+    while (curr !== null) {
+        arr.push(curr.val);
+        curr = curr.next;
+    }
+    let left = 0, right = arr.length - 1;
+    while (left < right) {
+        if (arr[left++] !== arr[right--]) return false;
+    }
+    return true;
+};
+    
